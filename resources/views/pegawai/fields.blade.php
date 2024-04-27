d5169513f20d84f244d1993bfe992a85dba2f6ec
Arman <trisaribaru@gmail.com>
2024-04-27 13:12:22 +0700

    fgix placeholder

--- resources/views/pegawai/fields.blade.php
+++ resources/views/pegawai/fields.blade.php
@@ -46,25 +46,25 @@
 <!-- tanggal lahir Field -->
 <div class="form-group col-md-6 mb-5">
     {{ Form::label('tanggal_lahir',__('messages.doctor.dob').':' ,['class' => 'form-label']) }}
-    {{ Form::text('tanggal_lahir', null,['class' => 'form-control doctor-dob','placeholder' => __('messages.doctor.dob'), 'id'=>'dob']) }}
+    {{ Form::text('tanggal_lahir', null,['class' => 'form-control doctor-dob','placeholder' => 'Tanggal Lahir', 'id'=>'dob']) }}
 </div>
 
 <!-- phpne Field -->
 <div class="form-group col-md-6 mb-5">
     {{ Form::label('phone', 'Nomor Telephone' ,['class' => 'form-label']) }}
-    {{ Form::text('phone', null,['class' => 'form-control ','placeholder' => __('messages.doctor.dob'), 'id'=>'dob']) }}
+    {{ Form::text('phone', null,['class' => 'form-control ','placeholder' => 'Nomor Telephone', 'id'=>'dob']) }}
 </div>
 
 <div class="col-md-6 mb-5">
     {{ Form::label('gender', 'Jenis Kelamin', ['class' => 'form-label']) }}
     <span class="required"></span>
-    {{ Form::select('gender', $genders, null, ['class' => 'form-select', 'placeholder' => __('Poli'), 'id' => 'medicineCategoryId']) }}
+    {{ Form::select('gender', $genders, null, ['class' => 'form-select', 'placeholder' => 'Jenis Kelamin', 'id' => 'medicineCategoryId']) }}
 </div>
 
 <div class="col-md-6 mb-5">
     {{ Form::label('divisi', 'Divisi', ['class' => 'form-label']) }}
     <span class="required"></span>
-    {{ Form::select('divisi', $divisis, null, ['class' => 'form-select', 'placeholder' => __('Poli'), 'id' => 'medicineCategoryId']) }}
+    {{ Form::select('divisi', $divisis, null, ['class' => 'form-select', 'placeholder' => 'Divisi', 'id' => 'medicineCategoryId']) }}
 </div>
 
 
